f22561a6d32cbef31ed53495227361f8fedf18a0
Vinícius Gomes <vinigc19@gmail.com>
2020-11-24 20:46:26 -0300

    Get Turmas in ActiviyPage

--- src/pages/Activity/index.tsx
+++ src/pages/Activity/index.tsx
@@ -1,14 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import Navbar from '../../components/Navbar';
 import Select from '../../components/Select';
+import Classes from '../../models/Class';
+import api from '../../services/api';
 
 import './styles.css';
 
 const Activy = () => {
-
     const [subject, setSubject] = useState('');
+
+    const [value, setValue] = useState(0);
+    const [classes, setClasses] = useState<Classes[]>([]);
+    const [date, setDate] = useState();
+
+    useEffect(() => {
+        api.get<Classes[]>('turma/listar').then(response => {
+            const classes = response.data.map(classes => classes);
+            setClasses(classes);
+        })
+    }, [])
 
     return (
         <>
@@ -19,6 +31,7 @@ const Activy = () => {
                         label="Valor"
                         name="turma"
                         auxText="Valor"
+                        onChange={(e) => ( setValue(Number(e.target.value)) )}
                     />
 
                     <Select
@@ -26,12 +39,17 @@ const Activy = () => {
                         onChange={(e) => { setSubject(e.target.value) }}
                         name="subject"
                         label="Turma"
-                    />
+                    >
+                        {classes.map(classes => (
+                            <option key={classes.nome} value={classes.nome}>{classes.nome}</option>
+                        ))}
+                    </Select>
 
                     <Input
                         label="Data"
                         name="data"
                         auxText="Data"
+                        
                     />
                 </div>
                 <Button label="Salvar" func={() => { }}></Button>
