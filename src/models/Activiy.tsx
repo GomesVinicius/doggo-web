d05894ebd335ec3091d7a115dbcf1eed244f0258
Vinícius Gomes <vinigc19@gmail.com>
2020-11-24 20:41:12 -0300

    Models Created

---
+++ src/models/Activiy.tsx
@@ -0,0 +1,7 @@
+import Classes from "./Class";
+
+export default interface Activity {
+    valor: number,
+    turma: Classes,
+    data: Date
+};
---
+++ src/models/Rate.tsx
@@ -0,0 +1,7 @@
+import Activity from "./Activiy";
+import Classes from "./Class";
+
+export default interface Rate {
+    atividade: Activity,
+    turma: Classes
+};
